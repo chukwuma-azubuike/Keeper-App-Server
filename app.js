03ea96fc250e8b4211f8f13774da47fdddb303fd
soflarity <55975006+soflarity@users.noreply.github.com>
2021-03-31 11:54:18 +0100

    Remove route modules

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
-const createError = require('http-errors');
+require('dotenv').config();
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const usersRouter = require('./routes/users');
-const login = require('./routes/login');
-const signUp = require('./routes/sign-up')
+const ejs = require('ejs');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const session = require('express-session');
@@ -14,51 +9,24 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const FacebookStrategy = require('passport-facebook').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
 app.use(cors({
   origin: 'http://localhost:3000',
   // methods: 'GET, HEAD, PUT, PATCH, POST, DELETE'
   credentials: true
 }));
 
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser('My Big little secret'));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-
-app.use('/users', usersRouter);
-app.use('/login', login);
-app.use('/signup', signUp);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(session({
   secret: 'My Big little secret',
@@ -69,19 +37,19 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-// passport.use(new GoogleStrategy({
-//   clientID: process.env.GOOGLE_CLIENT_ID,
-//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//   callbackURL: process.env.GOOGLE_CALLBACK_URL,
-//   userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
-// },
-//   function (accessToken, refreshToken, profile, cb) {
-//     console.log(profile);
-//     User.findOrCreate({ googleId: profile.id }, function (err, user) {
-//       return cb(err, user);
-//     });
-//   }
-// ));
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK_URL,
+  userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
+},
+  function (accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 // passport.use(new FacebookStrategy({
 //   clientID: process.env.FACEBOOK_APP_ID,
@@ -136,23 +104,23 @@ app.get('/home', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-// app.post('/signup', (req, res) => {
-//   // const userName = req.body.username;
-//   // const password = req.body.password;
+app.post('/signup', (req, res, next) => {
+  // console.log(req.body)
+  res.send('Holla');
 
-//   console.log(req.body)
+  const { username, password } = req.body;
 
-//   // User.register({ username: userName, password: password }, (err, userFound) => {
-//   //   err ? res.send({
-//   //     message: err
-//   //   }) :
-//   //     passport.authenticate('local')(req, res, () => {
-//   //       res.send({
-//   //         message: 'Signed up successfully!'
-//   //       })
-//   //     })
-//   // })
-// })
+  User.register({ username: username, password: password }, (err, userCreated) => {
+    err ? res.send({
+      message: err
+    }) : console.log('registered')
+    passport.authenticate('local')(req, res, () => {
+      res.send({
+        message: 'Signed up successfully!'
+      })
+    })
+  })
+})
 
 // app.get('/users', function(req, res, next) {
 //   res.send('respond with a resource');
--- routes/sign-up.js
+++ routes/sign-up.js
@@ -2,22 +2,22 @@ var express = require('express');
 const passport = require('passport');
 var router = express.Router();
 
-router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('Holla')
+router.post('/', (req, res, next) => {
+    // console.log(req.body)
+    res.send('Holla');
 
     const { username, password } = req.body;
 
-    // User.register({ username: username, password: password }, (err, userFound) => {
-    //     err ? res.send({
-    //         message: err
-    //     }) :
-    //         passport.authenticate('local')(req, res, ()=>{
-    //             res.send({
-    //                 message: 'Signed up successfully!'
-    //             })
-    //         })
-    // })
+    User.register({ username: username, password: password }, (err, userCreated) => {
+        err ? res.send({
+            message: err
+        }) : console.log('registered')
+            passport.authenticate('local')(req, res, ()=>{
+                res.send({
+                    message: 'Signed up successfully!'
+                })
+            })
+    })
 })
 
 module.exports = router;
